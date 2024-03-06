f6a68935b2529bde61532310845cbc2d30f24515
Patrick Kariuki <patmangu2018@gmail.com>
2024-03-06 11:14:48 -0500

    messaging system complete solution

--- Tech-Lab-On-Campus/Topic-Exchange/solution/consumer_sol.py
+++ Tech-Lab-On-Campus/Topic-Exchange/solution/consumer_sol.py
@@ -1,12 +1,32 @@
-from consumer_interface import mqConsumerInterface
-import pika
+# Copyright 2024 Bloomberg Finance L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 
+import pika
+from consumer_interface import mqConsumerInterface
+
+
 class mqConsumer(mqConsumerInterface):
-    def __init__(self, binding_key, exchange_name, queue_name):
-        self.binding_key = binding_key
-        self.exchange_name = exchange_name
-        self.queue_name = queue_name
+    def __init__(
+        self, binding_key: str, exchange_name: str, queue_name: str
+    ) -> None:
+        # Save parameters to class variables
+        self.m_binding_key = binding_key
+        self.m_queue_name = queue_name
+        self.m_exchange_name = exchange_name
+        # Call setupRMQConnection
         self.setupRMQConnection()
 
     def setupRMQConnection(self) -> None:
@@ -18,44 +38,37 @@ class mqConsumer(mqConsumerInterface):
         self.m_channel = self.m_connection.channel()
 
         # Create Queue if not already present
-        self.m_channel.queue_declare(queue=self.queue_name)
+        self.m_channel.queue_declare(queue=self.m_queue_name)
 
         # Create the exchange if not already present
-        self.m_channel.exchange_declare(self.exchange_name)
+        self.m_channel.exchange_declare(self.m_exchange_name,exchange_type="topic")
 
         # Bind Binding Key to Queue on the exchange
         self.m_channel.queue_bind(
-            queue=self.queue_name,
-            routing_key=self.binding_key,
-            exchange=self.exchange_name,
+            queue=self.m_queue_name,
+            routing_key=self.m_binding_key,
+            exchange=self.m_exchange_name,
         )
 
         # Set-up Callback function for receiving messages
         self.m_channel.basic_consume(
-            self.queue_name, self.on_message_callback, auto_ack=False
+            self.m_queue_name, self.on_message_callback, auto_ack=False
         )
 
     def on_message_callback(
         self, channel, method_frame, header_frame, body
     ) -> None:
-        # Acknowledge message
+        # Acknowledge Message 
         channel.basic_ack(method_frame.delivery_tag, False)
+
         # Print Message
         print(f" [x] Received Message: {body}")
 
+       
     def startConsuming(self) -> None:
+        
         # Print " [*] Waiting for messages. To exit press CTRL+C"
-        print(' [*] Waiting for messages. To exit press CTRL+C')
+        print(" [*] Waiting for messages. To exit press CTRL+C")
+
         # Start consuming messages
         self.m_channel.start_consuming()
-
-    
-    def __del__(self) -> None:
-        # Print "Closing RMQ connection on destruction"
-        print('Closing RMQ connection on destruction')
-        
-        # Close Channel
-        self.m_channel.close()
-
-        # Close Connection
-        self.m_connection.close()
